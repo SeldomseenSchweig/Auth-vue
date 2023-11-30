c5302cbfe6b7363cda242cf64cef9450623da4cf
SeldomseenSchweig <jamesschweig@gmail.com>
2023-11-30 10:22:23 -0800

    created sign in and register

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { createStore } from "vuex";
+import router from "../router";
+import { auth } from "../firebase";
 
 export default createStore({
   state: {
@@ -13,8 +20,58 @@ export default createStore({
     },
   },
   actions: {
-    async login() {},
-    async logout() {},
-    async register() {},
+    async login({ commit }, details) {
+      const { email, password } = details;
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/user-not-found":
+            alert("User Not Found");
+            break;
+          case "auth/wrong-password":
+            alert("Wrong Password");
+            break;
+          default:
+            alert("something went wrong");
+        }
+        return;
+      }
+      commit("SET_USER", auth.currentUser);
+
+      router.push("/");
+    },
+    async logout({ commit }) {
+      await signOut(auth);
+      commit("CLEAR_USER");
+      router.push("login");
+    },
+    async register({ commit }, details) {
+      const { email, password } = details;
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/email-already-in-use":
+            alert("Email already in use");
+            break;
+          case "auth/invalid-email":
+            alert("Invalid email");
+            break;
+          case "auth/operation not allowed":
+            alert("Operation Not allowed");
+            break;
+          case "auth/weak-password":
+            alert("Weak Password");
+            break;
+          default:
+            alert("something went wrong");
+        }
+        return;
+      }
+      commit("SET_USER", auth.currentUser);
+
+      router.push("/");
+    },
   },
 });
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="login">
     <section class="forms">
-      <form class="register">
+      <form class="register" @submit.prevent="register">
         <h2>Register</h2>
         <input
           type="email"
@@ -16,8 +16,19 @@
         <input type="submit" value="Register" />
       </form>
 
-      <form class="login">
+      <form class="login" @submit.prevent="login">
         <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="email address"
+          v-model="login_form.email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="login_form.password"
+        />
+        <input type="submit" value="Login" />
       </form>
     </section>
   </main>
@@ -25,15 +36,26 @@
 
 <script>
 import { ref } from "vue";
-// import { useStore } from "vuex";
+import { useStore } from "vuex";
 export default {
   setup() {
     const login_form = ref({});
     const register_form = ref({});
-    // const store = useStore();
+    const store = useStore();
+
+    const login = () => {
+      store.dispatch("login", login_form.value);
+    };
+
+    const register = () => {
+      store.dispatch("register", register_form.value);
+    };
+
     return {
       login_form,
       register_form,
+      login,
+      register,
     };
   },
 };
